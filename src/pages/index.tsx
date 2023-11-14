abc5f0e984c86b01c06021729edc746ce4e4d08f
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-11-15 02:53:03 +0800

    add admin dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import { fontFamily } from "tailwindcss/defaultTheme";
 
 module.exports = {
   darkMode: ["class"],
