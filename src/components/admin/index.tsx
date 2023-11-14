b15f5345a55f5c802235781d23ab2a6abf085206
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2023-11-15 07:55:12 +0800

    fix user list query

--- src/components/admin/index.tsx
+++ src/components/admin/index.tsx
@@ -1,11 +1,7 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import { PrismaClient } from "@prisma/client";
 
-import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
-
-// import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
-
-import { api } from "~/utils/api";
+// import { api } from "~/utils/api";
 
 export default async function DisplayUserList() {
   const prisma = new PrismaClient();
@@ -19,8 +15,12 @@ export default async function DisplayUserList() {
       <div className=" flex gap-3 border-4 border-slate-400 bg-[#dfd8d800] p-4">
         <div className="flex flex-col text-3xl">Admin View</div>
         <div className="flex flex-col">
-          <p className="flex text-2xl">User List:</p>
-          <div></div>
+          <p className="flex text-lg">User Accounts:</p>
+          <div>
+            {allUsers.map((user) => (
+              <div key={user.id}>{`ID: ${user.id}`}</div>
+            ))}
+          </div>
         </div>
       </div>
     </>
