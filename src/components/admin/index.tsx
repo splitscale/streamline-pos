582c2fef212f250c036c0a9c87cef1110e626462
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2023-11-15 23:05:48 +0800

    Add display user list via admin panel
    Display of user list should work now

--- src/components/admin/index.tsx
+++ src/components/admin/index.tsx
@@ -1,8 +1,44 @@
-"use client"
+"use client";
 
-export default async function DisplayUserList(userData: any) {
-
-  const allUsers = userData;
+export default function DisplayUserList(userData: any) {
+  const allUsers = [
+    {
+      id: "1e750688-87ba-4d6d-9401-7fe28441388d",
+      created: "2023-11-15T14:17:37.000+00:00",
+      edited: "2023-11-15T14:17:37.000+00:00",
+      firstName: null,
+      lastName: null,
+      photoUrl: null,
+      email: null,
+    },
+    {
+      id: "791ce8e1-2470-42c1-a1ce-948adf679121",
+      created: "2023-11-15T14:17:42.000+00:00",
+      edited: "2023-11-15T14:17:42.000+00:00",
+      firstName: null,
+      lastName: null,
+      photoUrl: null,
+      email: null,
+    },
+    {
+      id: "c321d77b-acec-4c27-bd71-e5913c1dc16c",
+      created: "2023-11-15T14:17:22.000+00:00",
+      edited: "2023-11-15T14:17:22.000+00:00",
+      firstName: null,
+      lastName: null,
+      photoUrl: null,
+      email: null,
+    },
+    {
+      id: "cc10bfd4-9fc8-484d-8d1c-9ddd2068d0ab",
+      created: "2023-11-15T14:17:32.000+00:00",
+      edited: "2023-11-15T14:17:32.000+00:00",
+      firstName: null,
+      lastName: null,
+      photoUrl: null,
+      email: null,
+    },
+  ];
 
   console.log(allUsers);
 
