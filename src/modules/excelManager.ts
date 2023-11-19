81ceede220eee3d0c24d462fd21fec162bd97cb5
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-11-20 04:03:04 +0800

    refined inventory and mobile view

--- src/modules/excelManager.ts
+++ src/modules/excelManager.ts
@@ -1,25 +1,26 @@
 import Excel from "exceljs";
-export async function excelToJSON(filename: string) {
+export async function excelToJSON(filename?: string, buffer?: Buffer) {
   const workbook = new Excel.Workbook();
-  await workbook.xlsx.readFile(filename);
-  const jsonString =  JSON.stringify(workbook.worksheets[0]?.model) 
-  const rows = JSON.parse(jsonString)['rows'] as any[];
+
+  if (filename) await workbook.xlsx.readFile(filename);
+  if (buffer) await workbook.xlsx.load(buffer);
+
+  const jsonString = JSON.stringify(workbook.worksheets[0]?.model);
+  const rows = JSON.parse(jsonString)["rows"] as any[];
 
   const res: any[] = [];
 
   for (let i = 1; i < rows.length; i++) {
-    const cells = rows[i]['cells'];
-    
+    const cells = rows[i]["cells"];
+
     const mapped = {
-      name: cells[0]['value'] as string, 
-      price: cells[1]['value'] as number, 
-      availableUnits: cells[2]['value'] as number,
-    }
+      name: cells[0]["value"] as string,
+      price: cells[1]["value"] as number,
+      availableUnits: cells[2]["value"] as number,
+    };
 
     res.push(mapped);
   }
-
- 
 
   return res;
 }
